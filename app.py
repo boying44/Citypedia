589e632fd244d133354884506bd300e28f00f2b8
igerstein <isaac.gerstein1@gmail.com>
2015-11-10 15:42:25 -0500

    Added info.html and set up basic flask routes

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import urllib2,json
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-weatherAPI = 7243b666b6841ed373ea8cd1289cc06d
+weatherAPI = "7243b666b6841ed373ea8cd1289cc06d"
 
 @app.route("/")
 def home():
@@ -14,6 +14,14 @@ def home():
 	# result = request.read()
 	# r = json.loads(result)
 	return render_template("home.html")
+
+@app.route("/info", methods = ["GET", "POST"])
+def info():
+        if request.form.has_key("city") and request.form["city"] != "":
+                city = request.form["city"]
+                return render_template("info.html", city = city)
+        else:
+                return redirect("/")
 
 if __name__ == "__main__":
    app.debug = True

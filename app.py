d5277c908326637f38fa0fbeea4693c4de3b0226
igerstein <isaac.gerstein1@gmail.com>
2015-11-15 10:42:52 -0500

    Moved information display to info page

--- app.py
+++ app.py
@@ -6,28 +6,28 @@ app = Flask(__name__)
 weatherAPI = "7243b666b6841ed373ea8cd1289cc06d"
 
 @app.route("/")
-def home(city=""):
-	url = """
-http://api.openweathermap.org/data/2.5/weather?q=%s&units=Imperial&appid=%s
-	"""
-	url = url%(city, weatherAPI)
-	request = urllib2.urlopen(url)
-	result = request.read()
-	r = json.loads(result)
-
-	main = r["main"]
-	info = []
-	info.append("Current Temperature:" + str(main["temp"]))
-	info.append("Min Temperature:" + str(main["temp_min"]))
-	info.append("Max Temperature:" + str(main["temp_max"]))
-	info.append("Humidity:" + str(main["humidity"]))
-	return render_template("home.html", weather = info)
+def home():
+	return render_template("home.html")
 
 @app.route("/info", methods = ["GET", "POST"])
 def info():
         if request.form.has_key("city") and request.form["city"] != "":
                 city = request.form["city"]
-                return render_template("info.html", city = city)
+
+                #Weather data
+                url = "http://api.openweathermap.org/data/2.5/weather?q=%s&units=Imperial&appid=%s"
+                url = url%(city.replace(" ", "%20"), weatherAPI)
+                req = urllib2.urlopen(url)
+                result = req.read()
+                r = json.loads(result)
+                main = r["main"]
+                weather = []
+                weather.append("Current Temperature:" + str(main["temp"]))
+                weather.append("Min Temperature:" + str(main["temp_min"]))
+                weather.append("Max Temperature:" + str(main["temp_max"]))
+                weather.append("Humidity:" + str(main["humidity"]))
+                
+                return render_template("info.html", city = city, weather = weather)
         else:
                 return redirect("/")
 
